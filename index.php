21c7156fe6769acf285ac92956efdaeed1a53db2
Mason Sain <sainmason012@gmail.com>
2024-04-19 04:17:46 -0400

    Added view pages

--- index.php
+++ index.php
@@ -14,11 +14,20 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>Pet Home</h1>';
+    //echo '<h1>Pet Home</h1>';
 
     // Render a view page
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Define order pet route
+$f3->route('GET /order', function() {
+   //echo '<h1>Order Pet</h1>';
+
+   //Render view page
+    $view = new Template();
+    echo $view->render('views/pet-order.html');
 });
 
 // Run Fat-Free
